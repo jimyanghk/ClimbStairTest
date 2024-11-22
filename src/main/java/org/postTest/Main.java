ea817496ba4899ef29c202a32f77c35a0ad25b45
jimyanghk <jimyanghk@gmail.com>
2024-11-22 10:02:19 +0800

    Add files via upload
    bug fix in program

--- src/main/java/org/postTest/Main.java
+++ src/main/java/org/postTest/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         ClimbStair climbStair=new ClimbStair();
         try {
-            BigInteger ways= ways = climbStair.calculateWays(5);
+            BigInteger ways = climbStair.calculateWays(5);
             System.out.println(ways);
         } catch (InvalidInputException e) {
             System.out.println(e.getMessage());
